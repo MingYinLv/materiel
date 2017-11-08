06a63978e61ce69bd0edaedbd90230e7fe31fed0
lvmingyin <lvmingyin@vip.qq.com>
2017-11-08 09:48:42 +0800

    aes

--- main/test.go
+++ main/test.go
@@ -3,7 +3,21 @@ package main
 import (
 	"materiel/src/util"
 	"fmt"
+	"encoding/hex"
 )
 
 func main() {
+	aesEnc := util.AesEncrypt{}
+	arrEncrypt, err := aesEnc.Encrypt("1")
+	if err != nil {
+		fmt.Println(arrEncrypt)
+		return
+	}
+	fmt.Println(hex.EncodeToString(arrEncrypt))
+	strMsg, err := aesEnc.Decrypt(arrEncrypt)
+	if err != nil {
+		fmt.Println(arrEncrypt)
+		return
+	}
+	fmt.Println(strMsg)
 }
