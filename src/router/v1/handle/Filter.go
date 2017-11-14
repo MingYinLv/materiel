4ca8f044fe51f53d608cc529555db53630e368f2
lvmingyin <lvmingyin@vip.qq.com>
2017-11-14 14:10:08 +0800

    sleep and rm

--- src/router/v1/handle/Filter.go
+++ src/router/v1/handle/Filter.go
@@ -1,9 +1,15 @@
 package handle
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"time"
+)
 
 func Cors(c *gin.Context) {
 	c.Header("Access-Control-Allow-Origin", "*")
 	c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT")
 	c.Header("Access-Control-Allow-Headers", "x-requested-with, content-type, authorization")
+	// 所有请求暂停2秒
+	time.Sleep(time.Second * 2)
+
 }
